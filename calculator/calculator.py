a64b63c7de67fc314f5c0b2487e918a7661858d6
Alex Yang <alexyoung0204@gmail.com>
2024-07-02 11:14:40 -0400

    updating m09 class coding files

---
+++ calculator/calculator.py
@@ -0,0 +1,3 @@
+def adding_nums(num1, num2):
+    result = num1 + num2
+    return result
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='demo',
+      version='0.1',
+      description='a simple math calculator package',
+      author='Alex Yang',
+      author_email='zy7ts@virginia.edu',
+      license='MIT',
+      packages=['calculator']
+      )
